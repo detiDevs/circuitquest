9896666b26537f94cdafede4bc5de741bb70ea6c
Luca Steinhausen <lucasteinhausen@web.de>
2026-02-11 17:32:36 +0100

    Removed unused files, fixed output -> outValue

--- lib/core/components/gates/nand_gate.dart
+++ lib/core/components/gates/nand_gate.dart
@@ -3,7 +3,7 @@ import 'package:circuitquest/core/components/gates/binary_gate.dart';
 class NandGate extends BinaryGate {
   @override
   int compute(int a, int b) {
-    final mask = (1 << outputs["output"]!.bitWidth) - 1;
+    final mask = (1 << outputs["outValue"]!.bitWidth) - 1;
     return ~(a & b) & mask;
   }
 }
